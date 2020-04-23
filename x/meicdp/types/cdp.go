66517a24e5f617aa7489b779b06d97c3a7bd79ab
Thara <57070054@kmitl.ac.th>
2020-04-23 17:16:19 +0700

    Remove unused

--- x/meicdp/types/cdp.go
+++ x/meicdp/types/cdp.go
@@ -4,9 +4,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// RouterKey is they name of the bank module
-const RouterKey = ModuleName
-
 //CDP is a Collateralized Debt Position of an user account
 type CDP struct {
 	CollateralAmount sdk.Coins      `json:"collateralAmount"`
