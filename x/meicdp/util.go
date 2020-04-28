0e692b175b3339d32c8f16a31dfaad34607eea8d
Thara <57070054@kmitl.ac.th>
2020-04-28 15:51:45 +0700

    Mock collateral

--- x/meicdp/util.go
+++ x/meicdp/util.go
@@ -8,14 +8,14 @@ import (
 
 func calculateCollateralRatio(discountedCollateralValue *big.Float, totalDebtAmount *big.Float) *big.Float {
 	// // TODO: calculate ratio
-	toPercent := new(big.Float).SetFloat64(100)
+	// toPercent := new(big.Float).SetFloat64(100)
 
-	collateralRatio := new(big.Float)
-	collateralRatio.Quo(discountedCollateralValue, totalDebtAmount)
-	collateralRatio.Mul(collateralRatio, toPercent)
+	// collateralRatio := new(big.Float)
+	// collateralRatio.Quo(discountedCollateralValue, totalDebtAmount)
+	// collateralRatio.Mul(collateralRatio, toPercent)
 
 	//Mock
-	// collateralRatio := new(big.Float).SetFloat64(151)
+	collateralRatio := new(big.Float).SetFloat64(151)
 	return collateralRatio
 }
 
