ca5443035cee48e57b138fec619f11bb56532f7f
Thara <57070054@kmitl.ac.th>
2020-05-04 19:27:30 +0700

    Fixed compare bar

--- ui/src/components/CompareBar.js
+++ ui/src/components/CompareBar.js
@@ -10,14 +10,23 @@ const Bar = styled(Flex).attrs(({ width, bg, height }) => ({
   alignItems: 'center',
 }))`
   border-radius: ${(props) => props.height};
+  position: absolute;
+  left: 0;
+  top: ${(props) => props.top || '0'};
+`
+
+const Container = styled(Flex).attrs(() => ({
+  my: '1.4vw',
+  width: '100%',
+  height: '1.389vw',
+}))`
+  position: relative;
 `
 
 export default ({ debt, maxDebt, collateral }) => (
-  <Flex width="100%" my="1.4vw">
-    <Bar width="100%" height="2.083vw" bg={colors.gray.normal}>
-      <Bar width="66.6%" height="1.389vw" bg={colors.pink.normal}>
-        <Bar width="33.3%" height="0.694vw" bg={colors.pink.dark} />
-      </Bar>
-    </Bar>
-  </Flex>
+  <Container>
+    <Bar width="100%" height="2.083vw" bg={colors.gray.normal} />
+    <Bar width="66.6%" height="1.389vw" bg={colors.pink.normal} top="0.3vw" />
+    <Bar width="33.3%" height="0.694vw" bg={colors.pink.dark} top="0.6vw" />
+  </Container>
 )
