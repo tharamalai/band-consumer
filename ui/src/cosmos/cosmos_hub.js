5eef81c6e1bbd481cb2b8f3b1d9abed25e61cb20
Thara <57070054@kmitl.ac.th>
2020-05-05 14:59:57 +0700

    Get private key

--- ui/src/cosmos/cosmos_hub.js
+++ ui/src/cosmos/cosmos_hub.js
@@ -5,6 +5,8 @@ const  COSMOS_CHAIN_ID = "band-cosmoshub";
 
 // export const cosmosJs = null
 
+export let ecpairPriv = null
+
 export const initiateCosmosJs = () => {
   const cosmosJs = cosmosjs.network(COSMOS_LCD_URL, COSMOS_CHAIN_ID);
   return cosmosJs
@@ -13,9 +15,12 @@ export const initiateCosmosJs = () => {
 export const getCosmosAddress = (mnemonic) => {
   try {
     const cosmos = initiateCosmosJs()
+    cosmos.setPath("m/44'/118'/0'/0/0");
     const address = cosmos.getAddress(mnemonic);
+    ecpairPriv = cosmos.getECPairPriv(mnemonic);
     return address
   } catch (error) {
+    console.log(error)
     throw `Error cannot get cosmos account from mnemonic: ${error.message}`
   }
 }
