d4adb88738bcf86a9606ba1623a106f49b0db285
Thara <57070054@kmitl.ac.th>
2020-05-05 12:05:43 +0700

    Remove fixed Address

--- ui/src/hooks/cosmoshub.js
+++ ui/src/hooks/cosmoshub.js
@@ -2,6 +2,6 @@ import useAxios from 'axios-hooks'
 
 export const useCosmosBalance = (cosmosAddress) => {
   return useAxios(
-    `http://gaia-ibc-hackathon.node.bandchain.org:1317/bank/balances/${cosmosAddress}`,
+    `http://gaia-ibc-hackathon.node.bandchain.org:1317/bank/balances/${cosmosAddress}`, 
   )
 }
